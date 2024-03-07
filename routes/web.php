7facb1b9a5d58a76b9f098e3211b97a529185ed3
minbaby01 <ongbeo111@gmail.com>
2024-03-07 11:56:33 +0700

    #18 - fix conflict

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\DashboardController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -33,34 +34,23 @@ Route::prefix('admin')->group(function () {
         Route::get('/edit/{user}', [UserController::class, 'edit']);
         Route::post('/edit/{user}', [UserController::class, 'handleUpdate']);
         Route::delete('/', [UserController::class, 'handleDelete']);
-
     });
 
+    //Brands
+    Route::prefix('brands')->group(function () {
+        Route::get('/', [BrandController::class, 'home']);
 
-});
+        Route::get('/details/{brand}', [BrandController::class, 'details']);
 
-// Auth
-Route::prefix('auth')->group(function () {
-    Route::get('/logout', [AuthController::class, 'logout'])
-        ->name('logout');
+        Route::get('/create', [BrandController::class, 'create']);
+        Route::post('/create', [BrandController::class, 'handleCreate']);
 
-    Route::get('/login', [AuthController::class, 'login'])
-        ->middleware('checklogin::class');
+        Route::get('/edit/{brand}', [BrandController::class, 'edit']);
+        Route::post('/edit/{brand}', [BrandController::class, 'handleUpdate']);
 
-    Route::post('/login', [AuthController::class, 'store'])
-        ->name('login');
-
-    Route::get('/otp', [AuthController::class, 'OTPverify'])
-        ->name('OTPverify')
-        ->middleware('checklogin::class');
-
-    Route::post('/otp', [AuthController::class, 'OTPverify_POST'])
-        ->name('OTP_POST');
-
-    Route::get('/otp/resend', [AuthController::class, 'resendOtp'])
-        ->name('resendOtp');
-});
-
+        Route::delete('/', [BrandController::class, 'handleDelete']);
+    });
+    
     //categories
     Route::prefix('categories')->group(function () {
         Route::get('/', [CategoryController::class, 'home']);
