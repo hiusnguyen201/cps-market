c2fce9e5e9cd940af453ed7d8e4899cac42842b8
minbaby01 <ongbeo111@gmail.com>
2024-03-12 23:12:22 +0700

    #41 - complete  99.99%

--- resources/views/auth/socialUpdateInfo.blade.php
+++ resources/views/auth/socialUpdateInfo.blade.php
@@ -1,206 +1,115 @@
-<!DOCTYPE html>
-<html class="no-js" lang="en">
+@extends('layouts.customer.index')
 
-<head>
-    <meta charset="UTF-8">
-    <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge"><![endif]-->
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="images/favicon.png" rel="shortcut icon">
-    <title>Ludus - Electronics, Apparel, Computers, Books, DVDs & more</title>
+@section('content')
+<!--====== App Content ======-->
+<div class="app-content">
+    <!--====== Section 1 ======-->
+    <div class="u-s-p-y-60">
+        <!--====== Section Content ======-->
+        <div class="section__content">
+            <div class="container">
+                <div class="breadcrumb">
+                    <div class="breadcrumb__wrap">
+                        <ul class="breadcrumb__list">
+                            <li class="has-separator">
 
-    <!-- Google Font: Source Sans Pro -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
-    <!--====== Vendor Css ======-->
-    <link rel="stylesheet" href="{{ asset('ludus/css/vendor.css') }}">
+                                <a href="/">Home</a>
+                            </li>
+                            <li class="is-marked">
 
-    <!--====== Utility-Spacing ======-->
-    <link rel="stylesheet" href="{{ asset('ludus/css/utility.css') }}">
-
-    <!--====== App ======-->
-    <link rel="stylesheet" href="{{ asset('ludus/css/app.css') }}">
-
-</head>
-
-<body class="config">
-    <div class="preloader is-active">
-        <div class="preloader__wrap">
-
-            <img class="preloader__img" src="images/preloader.png" alt="">
-        </div>
-    </div>
-
-    <!--====== Main App ======-->
-    <div id="app">
-        <!--====== Main Header ======-->
-        <header class="header--style-1 header--box-shadow">
-            @include('layouts.customer.parts.navbar')
-        </header>
-        <!--====== End - Main Header ======-->
-
-
-        <!--====== App Content ======-->
-        <div class="app-content">
-            @yield('content')
-            <!--====== Section 1 ======-->
-            <div class="u-s-p-y-60">
-
-                <!--====== Section Content ======-->
-                <div class="section__content">
-                    <div class="container">
-                        <div class="breadcrumb">
-                            <div class="breadcrumb__wrap">
-                                <ul class="breadcrumb__list">
-                                    <li class="has-separator">
-
-                                        <a href="/">Home</a>
-                                    </li>
-                                    <li class="is-marked">
-
-                                        <a href="/auth/login">Sign in</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                                <a href="/auth/login">Sign in</a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
-            <!--====== End - Section 1 ======-->
-
-
-            <!--====== Section 2 ======-->
-            <div class="u-s-p-b-60">
-
-                <!--====== Section Intro ======-->
-                <div class="section__intro u-s-m-b-60">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="section__text-wrap">
-                                    <h1 class="section__heading u-c-secondary">Update info account</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--====== End - Section Intro ======-->
-
-
-                <!--====== Section Content ======-->
-                <div class="section__content">
-                    <div class="container">
-                        <div class="row row--center">
-                            <div class="col-lg-6 col-md-8 u-s-m-b-30">
-                                <div class="l-f-o">
-                                    <div class="l-f-o__pad-box">
-
-                                        <form class="l-f-o__form" method="POST">
-                                            @csrf
-
-                                            <div class="u-s-m-b-30">
-
-                                                <label class="gl-label" for="name">NAME *</label>
-
-                                                <input class="input-text input-text--primary-style" type="text" id="name" placeholder="Enter name" name="name" value="{{ old('name') ?? $name }}" required>
-                                                @error('name')
-                                                <span style="color: red">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-
-                                            <div class="u-s-m-b-30">
-
-                                                <label class="gl-label" for="phone">PHONE *</label>
-
-                                                <input class="input-text input-text--primary-style" type="tel" id="phone" placeholder="Enter phone" name="phone" value="{{ old('phone') }}" required>
-                                                @error('phone')
-                                                <span style="color: red">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-
-                                            <div class="u-s-m-b-30">
-
-                                                <label class="gl-label" for="email">E-MAIL *</label>
-
-                                                <input class="input-text input-text--primary-style" type="email" id="email" placeholder="Enter E-mail" name="email" value="{{ old('email') ?? $email }}" required>
-                                                @error('email')
-                                                <span style="color: red">{{ $message }}</span>
-                                                @enderror
-                                            </div>
-
-                                            @if (session('error'))
-                                            <div class="alert alert-danger">
-                                                {{ session('error') }}
-                                            </div>
-                                            @endif
-
-                                            <div class="gl-inline">
-                                                <div class="u-s-m-b-30">
-
-                                                    <button class="btn btn--e-transparent-brand-b-2" type="submit">Confirm</button>
-                                                </div>
-                                            </div>
-
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!--====== End - Section Content ======-->
-            </div>
-            <!--====== End - Section 2 ======-->
         </div>
-        <!--====== End - App Content ======-->
-
-
-        <!--====== Main Footer ======-->
-        @include('layouts.customer.parts.footer')
-
     </div>
-    <!--====== End - Main App ======-->
+    <!--====== End - Section 1 ======-->
 
 
-    <!--====== Google Analytics: change UA-XXXXX-Y to be your site's ID ======-->
-    <script>
-        window.ga = function() {
-            ga.q.push(arguments)
-        };
-        ga.q = [];
-        ga.l = +new Date;
-        ga('create', 'UA-XXXXX-Y', 'auto');
-        ga('send', 'pageview')
-    </script>
-    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    <!--====== Section 2 ======-->
+    <div class="u-s-p-b-60">
 
-    <!--====== Vendor Js ======-->
-    <script src="{{ asset('ludus/js/vendor.js') }}"></script>
-
-    <!--====== jQuery Shopnav plugin ======-->
-    <script src="{{ asset('ludus/js/jquery.shopnav.js') }}"></script>
-
-    <!--====== App ======-->
-    <script src="{{ asset('ludus/js/app.js') }}"></script>
-
-    <!--====== Noscript ======-->
-    <noscript>
-        <div class="app-setting">
+        <!--====== Section Intro ======-->
+        <div class="section__intro u-s-m-b-60">
             <div class="container">
                 <div class="row">
-                    <div class="col-12">
-                        <div class="app-setting__wrap">
-                            <h1 class="app-setting__h1">JavaScript is disabled in your browser.</h1>
-
-                            <span class="app-setting__text">Please enable JavaScript in your browser or upgrade to a JavaScript-capable browser.</span>
+                    <div class="col-lg-12">
+                        <div class="section__text-wrap">
+                            <h1 class="section__heading u-c-secondary">Update info account</h1>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </noscript>
-</body>
+        <!--====== End - Section Intro ======-->
 
-</html>
+
+        <!--====== Section Content ======-->
+        <div class="section__content">
+            <div class="container">
+                <div class="row row--center">
+                    <div class="col-lg-6 col-md-8 u-s-m-b-30">
+                        <div class="l-f-o">
+                            <div class="l-f-o__pad-box">
+
+                                <form class="l-f-o__form" method="POST">
+                                    @csrf
+
+                                    <div class="u-s-m-b-30">
+
+                                        <label class="gl-label" for="name">NAME *</label>
+
+                                        <input class="input-text input-text--primary-style" type="text" id="name" placeholder="Enter name" name="name" value="{{ old('name') ?? $name }}" required>
+                                        @error('name')
+                                        <span style="color: red">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="u-s-m-b-30">
+
+                                        <label class="gl-label" for="phone">PHONE *</label>
+
+                                        <input class="input-text input-text--primary-style" type="tel" id="phone" placeholder="Enter phone" name="phone" value="{{ old('phone') }}" required>
+                                        @error('phone')
+                                        <span style="color: red">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="u-s-m-b-30">
+
+                                        <label class="gl-label" for="email">E-MAIL *</label>
+
+                                        <input class="input-text input-text--primary-style" type="email" id="email" placeholder="Enter E-mail" name="email" value="{{ old('email') ?? $email }}" required>
+                                        @error('email')
+                                        <span style="color: red">{{ $message }}</span>
+                                        @enderror
+                                    </div>
+
+                                    @if (session('error'))
+                                    <div class="alert alert-danger">
+                                        {{ session('error') }}
+                                    </div>
+                                    @endif
+
+                                    <div class="gl-inline">
+                                        <div class="u-s-m-b-30">
+
+                                            <button class="btn btn--e-transparent-brand-b-2" type="submit">Confirm</button>
+                                        </div>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--====== End - Section Content ======-->
+    </div>
+    <!--====== End - Section 2 ======-->
+</div>
+<!--====== End - App Content ======-->
+@endsection
